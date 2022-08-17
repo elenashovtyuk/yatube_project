a80c5e660c4c2e0377a0ad5e3e7778b6e514ce87
Елена Шовтюк <shovtyuk.elena@yandex.ru>
2022-08-17 21:45:31 +0400

    extends, include, block

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -8,5 +8,7 @@ def index(request):
     template = 'posts/index.html'
     return render(request, template)
 
+
 def group_posts(request, slug):
-    return HttpResponse(f'Посты, отфильтрованные по группам {slug}')
+    template = 'posts/group_list.html'
+    return render(request, template)
