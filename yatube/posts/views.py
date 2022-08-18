51d4aebe02bdaeac282d2dfd173bd461b088803d
Елена Шовтюк <shovtyuk.elena@yandex.ru>
2022-08-18 14:21:01 +0400

    context dict in views

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -6,9 +6,21 @@ from django.http import HttpResponse
 
 def index(request):
     template = 'posts/index.html'
-    return render(request, template)
+    title = 'Yatube'
+    text = 'Это главная страница проекта Yatube'
+    context = {
+        'title': title,
+        'text': text
+    }
+    return render(request, template, context)
 
 
 def group_posts(request, slug):
     template = 'posts/group_list.html'
-    return render(request, template)
+    title = 'Yatube'
+    text = 'Здесь будет информация о проектах Yatube'
+    context = {
+        'title': title,
+        'text': text
+    }
+    return render(request, template, context)
