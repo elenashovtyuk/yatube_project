c61d894c97cfa46c884be2b830a0df5a2c0731d7
Елена Шовтюк <shovtyuk.elena@yandex.ru>
2022-08-21 15:59:08 +0400

    task 3 sprint

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -1,10 +1,19 @@
-from django.db import models
 
 # Create your models here.
+# Для работы с модулями
 from django.db import models
+# Для создания поля со ссылкой на модель User
 from django.contrib.auth import get_user_model
-
+# обращение к модели User
 User = get_user_model()
+
+class Group(models.Model):
+    def __str__(self):
+        return self.title
+
+    title = models.CharField(max_length=200)
+    slug = models.SlugField()
+    description = models.TextField()
 
 
 class Post(models.Model):
@@ -13,5 +22,12 @@ class Post(models.Model):
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
+        related_name='posts'
+    )
+    group = models.ForeignKey(
+        Group,
+        blank=True,
+        null=True,
+        on_delete=models.PROTECT,
         related_name='posts'
     )
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,26 +1,40 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 from django.http import HttpResponse
 
 
 def index(request):
-    template = 'posts/index.html'
-    title = 'Yatube'
-    text = 'Это главная страница проекта Yatube'
+
+    #template = 'posts/index.html'
+    #title = 'Yatube'
+    #text = 'Это главная страница проекта Yatube'
+    #context = {
+        #'title': title,
+        #'text': text
+    #}
+    posts = Post.objects.order_by('-pub_date')[:10]
     context = {
-        'title': title,
-        'text': text
+        'posts': posts,
     }
-    return render(request, template, context)
+    return render(request, 'posts/index.html', context)
 
 
 def group_posts(request, slug):
-    template = 'posts/group_list.html'
-    title = 'Yatube'
-    text = 'Здесь будет информация о проектах Yatube'
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
     context = {
-        'title': title,
-        'text': text
+        'group': group,
+        'posts': posts,
+
     }
-    return render(request, template, context)
+    return render(request, 'posts/group_list.html', context)
+    #template = 'posts/group_list.html'
+    #title = 'Yatube'
+    #text = 'Здесь будет информация о проектах Yatube'
+    #context = {
+    #    'title': title,
+    #   'text': text
+    # }
+    #return render(request, template, context)
