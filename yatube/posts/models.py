56c3ca6d49577c4a5a77b5b66f1da83f99d4f7fd
Elena Shovtyuk <110084590+elenashovtyuk@users.noreply.github.com>
2022-08-23 13:37:53 +0400

    added unique slug

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -13,7 +13,7 @@ class Group(models.Model):
         return self.title
 
     title = models.CharField(max_length=200)
-    slug = models.SlugField()
+    slug = models.SlugField(unique=True)
     description = models.TextField()
 
 
